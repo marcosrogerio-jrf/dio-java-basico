beaf36d8f7f959779838b33999d467181604f90c
marcosrogerio-jrf <marcosrogerio.jrf@gmail.com>
2024-05-13 10:53:34 -0400

    finalizando o desafio

--- ContaBanco/src/desafio/ContaTerminal.java
+++ ContaBanco/src/desafio/ContaTerminal.java
@@ -1,7 +1,27 @@
 package desafio;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
 	public static void main(String[] args) {
-		System.out.println("Desafio Java Básico");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Desafio - Java Básico");
+		System.out.println("===============================================");
+		System.out.println("CONTA TERMINAL\n");
+		
+		System.out.print("Informe o número da conta: ");
+		int numero = scanner.nextInt();
+		
+		System.out.print("Informe o número da agência: ");
+		String agencia = scanner.next();
+		
+		System.out.print("Informe o nome do cliente: ");
+		String nomeCliente = scanner.next();
+		
+		System.out.print("Informe o saldo: ");
+		double saldo = scanner.nextDouble();
+		
+		
+		System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque.");
 	}
 }
